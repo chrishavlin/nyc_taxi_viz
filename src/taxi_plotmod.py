d1348ad41e7e6830d172693a6f9daec68d04bb5c
Chris Havlin <chris.havlin@gmail.com>
2016-10-06 09:18:46 -0600

    created new module for all plotting routines.

---
+++ src/taxi_plotmod.py
@@ -0,0 +1,270 @@
+"""---------------------------------------------------------------------------
+Plotting routines
+
+(c) Chris Havlin
+Open source license?
+----------------------------------------------------------------------------"""
+
+"""--------------
+Import libraries:
+-----------------"""
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+"""---------
+Classes
+------------"""
+class binned_variable(object):
+    """ class for variables binned by time """
+    def __init__(self,name,time_b,varmin=0,varmax=1):
+        self.varname = name # variable name
+        self.time_be = time_b # time bins, edge values
+        self.time_bc = (time_b[0:time_b.size-1]+time_b[1:time_b.size])/2.0 # time bin center values
+        self.varmin=varmin
+        self.varmax=varmax
+    
+    def bin_the_values(self,VarBig,Var_list):
+        # initialize the output 
+        self.mean=np.zeros((self.time_be.size-1,1))
+        self.med=np.zeros((self.time_be.size-1,1))
+        self.std=np.zeros((self.time_be.size-1,1))
+        self.N=np.zeros((self.time_be.size-1,1))
+
+        # extract variable of interest from full dataset
+        var0 = VarBig[:,Var_list.index(self.varname)]
+        time0 = VarBig[:,0]
+
+        # calculate standard deviation,mean,median and number of obs in each bin
+        for i_bin in range(self.time_bc.size):
+            var2bin = var0[time0>self.time_be[i_bin]]
+            time = time0[time0>self.time_be[i_bin]]
+    
+            var2bin = var2bin[time<self.time_be[i_bin+1]]
+    
+            var2bin = var2bin[var2bin>self.varmin]
+            var2bin = var2bin[var2bin<self.varmax]
+    
+            self.mean[i_bin]=var2bin.mean()
+            self.med[i_bin]=np.median(var2bin)
+            self.std[i_bin]=var2bin.std()
+            self.N[i_bin]=var2bin.size
+
+    def bin_hist(self,VarBig,Var_list,bin_edge1,bin_edge2):
+        # extract variable of interest from full dataset
+        var0 = VarBig[:,Var_list.index(self.varname)]
+        time0 = VarBig[:,0]
+
+        bin_var=var0[time0>bin_edge1]
+        time=time0[time0>bin_edge1]
+
+        bin_var=bin_var[time<bin_edge2]
+        bin_var = bin_var[bin_var>self.varmin]
+        bin_var = bin_var[bin_var<self.varmax]
+
+        self.hist_id=bin_var
+        self.hist_id_N=bin_var.size
+        self.hist_id_mean=bin_var.mean()
+        self.hist_id_med=np.median(bin_var)
+
+"""---------
+Functions
+------------"""
+def map_proc(VarBig,Var_list,bin_varname,bin_min,bin_max,Pickup,nx,ny,
+            bottom_left=[40.697089, -74.027397],top_left=[40.823067, -74.027397],
+            bottom_right=[40.697089, -73.914240],top_right=[40.823067,-73.914240]):
+   ##############################################################################
+   # Input Variables:
+   # VarBig
+   #    The 2-D array of all variables
+   # Var_list
+   #    The list identifying the columns of VarBig
+   # bin_varname
+   #    The name of the variable to bin
+   # bin_min, bin_max
+   #    The min/max value of the variable to bin   
+   # Pickup
+   #    Boolean true/false. If true, the variable of interest (bin_varname)
+   #    will be binned by the pick up location. If false, the drop off location. 
+   # nx,ny
+   #    Number of bin-nodes in x and y (i.e., number of bins in x is nx-1)
+   # bottom_left, top_left, etc.    
+   #    Grid dimensions: lat/lon points defining the corners of a rectangle
+   #
+   # Some useful lat/lon points	    
+   # Manhattan:
+   #        bottom_left=[40.697089, -74.027397],top_left=[40.823067, -74.027397],
+   #        bottom_right=[40.697089, -73.914240],top_right=[40.823067,-73.914240]
+   # Wall Street:
+   #        bottom_left=[40.701035, -74.022382],top_left=[40.715075, -74.022382],
+   #        bottom_right=[40.701035, -74.000207],top_right=[40.715075,-74.000207]
+   # Columbus Circle:
+   #        bottom_left=[40.765505, -73.985891],top_left=[40.770369, -73.985891],
+   #        bottom_right=[40.765505, -73.978744],top_right=[40.770369,-73.978744]
+   ##############################################################################
+
+    # build cell node grid
+    x=np.linspace(bottom_left[1],bottom_right[1],nx) # defined on cell corners
+    y=np.linspace(bottom_left[0],top_left[0],ny) # defined on cell corners
+
+    # create an empty TaxiCount array 
+    TotCount=np.zeros((ny-1,nx-1)) # defined on cell centers
+    MeanClr=np.zeros((ny-1,nx-1)) # defined on cell centers
+
+    # pull out lat/lon and variable lists depending on pickup or dropoff
+    if Pickup:
+       BigLat=VarBig[:,8] 
+       BigLon=VarBig[:,7] 
+    else:
+       BigLat=VarBig[:,10] 
+       BigLon=VarBig[:,9] 
+
+    ColorVar= VarBig[:,Var_list.index(bin_varname)]
+    indc=1
+
+    # count the number of points within each lat/lon bin
+    for ix in range(nx-1):
+        for iy in range(ny-1):
+            ymin=y[iy] # lat limit 1
+            ymax=y[iy+1] # lat limit 2
+            xmin=x[ix] # long limit 1 
+            xmax=x[ix+1] # long limit 2
+            
+            LocClr=ColorVar[np.logical_and(BigLon>xmin,BigLon<xmax)]
+            LocLat=BigLat[np.logical_and(BigLon>xmin,BigLon<xmax)]
+            
+            LocClr=LocClr[np.logical_and(LocLat>ymin,LocLat<ymax)]
+            LocClr=LocClr[np.logical_and(LocClr>bin_min,LocClr<bin_max)]
+
+            if len(LocClr)>0:
+               TotCount[iy,ix]=len(LocClr)
+               MeanClr[iy,ix]=LocClr.mean()
+               #print indc,'of',(nx*ny),'val:',TotCount[iy,ix]
+            #else:
+            #   #print indc,'of',(nx*ny),'val: empty'
+            indc=indc+1
+
+    return TotCount,MeanClr,x,y
+
+
+def plt_two_d_histogram(bin_varname,VarMin,VarMax,time_b,VarBig,Var_list):
+    
+    bin_inst=binned_variable(bin_varname,time_b,VarMin,VarMax)
+    bin_inst.bin_the_values(VarBig,Var_list)
+
+    # all values, limited by min,max
+
+    max_unbin=bin_inst.varmax
+    min_unbin=bin_inst.varmin
+
+    unbinned_value = VarBig[:,Var_list.index(bin_varname)]
+    time = VarBig[:,0]
+    
+    time = time[unbinned_value>=min_unbin]
+    unbinned_value = unbinned_value[unbinned_value>=min_unbin]
+    
+    time = time[unbinned_value<=max_unbin]
+    unbinned_value = unbinned_value[unbinned_value<=max_unbin]
+
+
+    clr = (0.3,0.3,0.3)
+    
+    plt.subplot(1,3,1)
+    plt.hist2d(time,unbinned_value,(48,30),cmap=cm.hot)
+    plt.colorbar()
+    plt.plot(bin_inst.time_bc,bin_inst.mean,color=(0.6,0.6,0.6),linewidth=3)
+    plt.plot(bin_inst.time_bc,bin_inst.med,color=clr,linewidth=3)
+    plt.xlim([0,24])
+    plt.ylim([min_unbin,max_unbin])
+    plt.ylim([min_unbin,50])
+    plt.ylabel(bin_varname)
+    plt.xlabel('time of day [24-hr]')
+    
+    plt.subplot(1,3,2)
+    bin_inst.bin_hist(VarBig,Var_list,5,6)
+    LAB='5-6,'+str(bin_inst.hist_id_N) + ',' + str(round(bin_inst.hist_id_med,1))
+    LAB = LAB + ',' + str(round(bin_inst.hist_id_mean,1))
+    plt.hist(bin_inst.hist_id,bins=50,histtype='step',normed=True,label=LAB)
+
+    bin_inst.bin_hist(VarBig,Var_list,9,18)
+    LAB='9-18,' +str(bin_inst.hist_id_N) + ',' + str(round(bin_inst.hist_id_med,1))
+    LAB = LAB + ',' + str(round(bin_inst.hist_id_mean,1))
+    plt.hist(bin_inst.hist_id,bins=50,histtype='step',normed=True,label=LAB)
+
+    bin_inst.bin_hist(VarBig,Var_list,20,24)
+    LAB='20-24,'+str(bin_inst.hist_id_N) + ',' + str(round(bin_inst.hist_id_med,1))
+    LAB = LAB + ',' + str(round(bin_inst.hist_id_mean,1))
+    plt.hist(bin_inst.hist_id,bins=50,histtype='step',normed=True,label=LAB)
+    plt.ylabel('bin N / tot N')
+
+    plt.xlabel(bin_varname)
+    plt.ylim([0,0.15])
+    plt.legend()
+    
+    plt.subplot(1,3,3)
+    plt.plot(bin_inst.time_bc,2*bin_inst.std)
+    plt.xlabel('time of day [24-hr]')
+    plt.ylabel('2-standard deviations of ' + bin_varname)
+    plt.xlim([0,24])
+
+    #LAB=str(unbinned_value.size) + ',' + str(round(np.median(unbinned_value),1))
+    
+    plt.figure()
+    plt.plot(bin_inst.N,bin_inst.med)
+    plt.scatter(bin_inst.N,bin_inst.med,c=bin_inst.time_bc)
+    plt.colorbar()
+    plt.xlabel('N')
+    plt.ylabel('speed [mph]')
+    plt.show()
+    
+    
+    #np.savetxt(write_dir+'/Vars.txt', Vars, delimiter=',')
+
+def plt_map(Zvar,minZ,maxZ,x,y):
+    plt.figure()
+
+    # mask the variable
+    Z = Zvar
+    #Z = np.ma.masked_array(Zvar,mask = (Zvar>minZ))
+    #Z = np.ma.masked_array(Z,mask = (Z<maxZ))
+    #cm.hot.set_bad('black', alpha=None)
+    
+    # cell edges
+    [Xgrid,Ygrid]=np.meshgrid(x,y) 
+
+    # cell centers
+    xc = (x[0:x.size-1]+x[1:x.size])/2
+    yc = (y[0:y.size-1]+y[1:y.size])/2
+    [Xgridc,Ygridc]=np.meshgrid(xc,yc)
+
+    pcol=plt.pcolormesh(Xgrid,Ygrid,Z,cmap=cm.hot,linewidth=0)
+    pcol.set_edgecolor('face')
+    plt.clim(minZ,maxZ)
+    plt.colorbar()
+    
+    # scatter plot
+    #nconts=300
+    #Z=np.multiply(TaxiCount,TaxiCount>minTax)
+    #Z=np.multiply(Z,Z<maxTax)
+    #plt.scatter(Xgridc,Ygridc,c=Z,s=100,cmap=cm.hot,edgecolors=None,linewidths=0)
+    #plt.clim(minTax,maxTax)
+    #plt.colorbar()
+    
+    # contour plot
+    #plt.subplot(1,2,1)
+    #nconts=400
+    #plt.contourf(Xgridc,Ygridc,TaxiCount,nconts,cmap=cm.hot)
+    #plt.contourf(Xgridc,Ygridc,TaxiCount,nconts,cmap=cm.hot)
+    #plt.clim(minTax,maxTax)
+    #plt.colorbar()
+    
+    # log plot
+    #nconts=300
+    #plt.subplot(1,2,2)
+    #plt.contourf(Xgridc,Ygridc,np.log10(TaxiCount),nconts,cmap=cm.hot)
+    #plt.clim(np.log10(minTax),np.log10(maxTax))
+    #plt.colorbar()
+    
+    plt.show()
+
+
